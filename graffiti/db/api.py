1954dae6cfee89f20dfd464698fa8e935abbb2e7
Wayne Okuma <wayne.okuma@hp.com>
2014-08-01 16:31:50 -0700

    Testing dependecy branch.
    Change-Id: Icfcd8e00a074ae2487f2231e949063125cfea901
    Implements: blueprint capability-type-persistence

--- graffiti/db/api.py
+++ graffiti/db/api.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# WKO: testing branch dependency
+
 # import copy
 from oslo.config import cfg
 
--- graffiti/db/models.py
+++ graffiti/db/models.py
@@ -12,6 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+# WKO: testing branch dependencies
+
 """
 SQLAlchemy Models for storing graffiti
 """
